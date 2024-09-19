2f7959f20a2f96e3193019816aca54b1a54dd9c7
na <161771692+joyllty@users.noreply.github.com>
2024-09-19 18:11:07 -0300

    Update and rename números pares (for, if) to números pares (for).c

--- linguagem c/números pares (for).c
+++ linguagem c/números pares (for).c
@@ -1,8 +1,6 @@
 /* Escreva um programa que mostre todos os números pares existentes entre 1 e um número lido do teclado, bem como a quantidade 
 de números apresentados */
-
 #include <stdio.h>
-
 int main() {
   int num, contador = 0, i;
   printf("Digite um número e o programa te dirá quantos números pares existem entre 1 e o seu número!: \n");
@@ -10,13 +8,12 @@ int main() {
 
   
   for(i = 1; i <= num; i ++){
-    
     if(i % 2 == 0){
       contador++;
       printf("\n%d", i);
-    
     }
   }
+  
   printf("\nA quantidades de números pares existentes é: %d", contador);
   
   return 0;
