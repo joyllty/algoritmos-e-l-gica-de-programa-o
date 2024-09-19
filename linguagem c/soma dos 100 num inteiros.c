7ec069cfc79b018923956253185f708fadd211e8
na <161771692+joyllty@users.noreply.github.com>
2024-09-19 18:45:05 -0300

    Update and rename soma dos 100 num inteiros to soma dos 100 num inteiros.c

--- linguagem c/soma dos 100 num inteiros.c
+++ linguagem c/soma dos 100 num inteiros.c
@@ -1,8 +1,6 @@
 /* Escreva um programa que mostre na tela a soma obtida dos 100 primeiros números inteiros (1+ 2 + 3 + 4 + 5 + ... + 100). 
 Resultado: 5050 */
-
 #include <stdio.h>
-
 int main() {
   int soma = 0, i;
   printf("Programa que soma os 100 primeiros números inteiros: \n");
@@ -11,8 +9,7 @@ int main() {
     soma = soma + i;
     printf("\n%d", i);
   }
-    
-
   printf("\nO resultado é: %d", soma);
+  
   return 0;
 }
