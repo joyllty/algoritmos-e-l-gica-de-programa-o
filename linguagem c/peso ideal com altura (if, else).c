35db4624b38f128a39426c5865f7339c9bcb14fb
na <161771692+joyllty@users.noreply.github.com>
2024-09-19 18:13:39 -0300

    Update and rename peso ideal com altura (if, else) to peso ideal com altura (if, else).c

--- linguagem c/peso ideal com altura (if, else).c
+++ linguagem c/peso ideal com altura (if, else).c
@@ -1,8 +1,6 @@
 /* Tendo como dados de entrada a altura e o gênero de uma pessoa, faça um programa que calcule o seu peso ideal, de acordo com as seguintes fórmulas:
 - Para homens: 72.7 x altura – 58- Para mulheres: 62.1 x altura - 44.7 */
-
 #include <stdio.h>
-
 int main() {
     float altura, p_ideal;
     char genero;
@@ -16,23 +14,20 @@ int main() {
     if(genero == 'M' || genero == 'm'){
         p_ideal = 72.7 * altura - 58;
         printf("SEXO MASCULINO");
-            printf("\nSeu peso ideal é: %.2f", p_ideal);
-                
+        printf("\nSeu peso ideal é: %.2f", p_ideal); 
     }
-        else if(genero == 'F' || genero == 'f'){
-            p_ideal = 62.1 * altura - 44.7;
-            printf("SEXO FEMININO");
-                printf("\nSeu peso ideal é: %.2f", p_ideal);
-                    
+    else if(genero == 'F' || genero == 'f'){
+        p_ideal = 62.1 * altura - 44.7;
+        printf("SEXO FEMININO");
+        printf("\nSeu peso ideal é: %.2f", p_ideal);   
     }
-            else if(genero == 'O' || genero == 'o'){
-                printf("OPÇÃO OUTRO");
-                    printf("\nNão temos a equação para esse gênero :(, mas já estamos em desenvolvimento! ");
-                
+    else if(genero == 'O' || genero == 'o'){
+        printf("OPÇÃO OUTRO");
+        printf("\nNão temos a equação para esse gênero :(, mas já estamos em desenvolvimento! ");
     }
-                else{
-                    printf("Opção inválida!\nRepita a operação.");
-                    
+    else{
+        printf("Opção inválida!\nRepita a operação.");
     }
+    
     return 0;
 }
